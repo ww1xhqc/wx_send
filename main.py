28813ed23fe6506a1e61d7a54108370aae2cfb5a
ww1xhqc <18258277831@163.com>
2025-01-23 10:49:45 +0800

    feat(): 微信每天定时推送

---
+++ main.py
@@ -0,0 +1,141 @@
+import time
+import requests
+import json
+import html5lib
+import schedule
+from bs4 import BeautifulSoup
+import os
+
+# # 从测试号信息获取
+# appID = "wx966de8558149bf3f"
+# appSecret = "0925ef3d32bfe5e907d7e672b4e9dd88"
+# # 收信人ID即 用户列表中的微信号，
+# openIds = ["o8_NG7F4seO45Pq2op-vwx05Prbs", "o8_NG7NEoFugvlcUWDRBdJHrBW6Q"]
+# # 天气预报模板ID
+# weather_template_id = "X20zchsleEyx00oRsLJJA8D5yXa4uVLLTQe4OdcSnU4"
+
+# 从环境变量中获取
+appID = os.environ.get("APP_ID")
+appSecret = os.environ.get("APP_SECRET")
+# 收信人ID即 用户列表中的微信号，
+openIds = os.environ.get("OPEN_IDS").split(',')
+# 天气预报模板ID
+weather_template_id = os.environ.get("WEATHER_TEMPLATE_ID")
+
+def get_weather(my_city):
+    urls = ["http://www.weather.com.cn/textFC/hb.shtml",
+            "http://www.weather.com.cn/textFC/db.shtml",
+            "http://www.weather.com.cn/textFC/hd.shtml",
+            "http://www.weather.com.cn/textFC/hz.shtml",
+            "http://www.weather.com.cn/textFC/hn.shtml",
+            "http://www.weather.com.cn/textFC/xb.shtml",
+            "http://www.weather.com.cn/textFC/xn.shtml"
+            ]
+    for url in urls:
+        resp = requests.get(url)
+        text = resp.content.decode("utf-8")
+        soup = BeautifulSoup(text, 'html5lib')
+        div_con_midtab = soup.find("div", class_="conMidtab")
+        tables = div_con_midtab.find_all("table")
+        for table in tables:
+            trs = table.find_all("tr")[2:]
+            for index, tr in enumerate(trs):
+                tds = tr.find_all("td")
+                # 这里倒着数，因为每个省会的td结构跟其他不一样
+                city_td = tds[-8]
+                this_city = list(city_td.stripped_strings)[0]
+                if this_city == my_city:
+                    high_temp_td = tds[-5]
+                    low_temp_td = tds[-2]
+                    weather_type_day_td = tds[-7]
+                    weather_type_night_td = tds[-4]
+                    wind_td_day = tds[-6]
+                    wind_td_day_night = tds[-3]
+
+                    high_temp = list(high_temp_td.stripped_strings)[0]
+                    low_temp = list(low_temp_td.stripped_strings)[0]
+                    weather_typ_day = list(weather_type_day_td.stripped_strings)[0]
+                    weather_type_night = list(weather_type_night_td.stripped_strings)[0]
+
+                    wind_day = list(wind_td_day.stripped_strings)[0] + list(wind_td_day.stripped_strings)[1]
+                    wind_night = list(wind_td_day_night.stripped_strings)[0] + list(wind_td_day_night.stripped_strings)[
+                        1]
+
+                    # 如果没有白天的数据就使用夜间的
+                    temp = f"{low_temp}——{high_temp}摄氏度" if high_temp != "-" else f"{low_temp}摄氏度"
+                    weather_typ = weather_typ_day if weather_typ_day != "-" else weather_type_night
+                    wind = f"{wind_day}" if wind_day != "--" else f"{wind_night}"
+                    return this_city, temp, weather_typ, wind
+
+
+def get_access_token():
+    # 获取access token的url
+    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}' \
+        .format(appID.strip(), appSecret.strip())
+    response = requests.get(url).json()
+    print(response)
+    access_token = response.get('access_token')
+    return access_token
+
+
+def get_daily_love():
+    # 每日一句情话
+    url = "https://api.lovelive.tools/api/SweetNothings/Serialization/Json"
+    r = requests.get(url)
+    all_dict = json.loads(r.text)
+    sentence = all_dict['returnObj'][0]
+    daily_love = sentence
+    print(daily_love)
+    return daily_love
+
+
+def send_weather(access_token, weather):
+    import datetime
+    today = datetime.date.today()
+    today_str = today.strftime("%Y年%m月%d日")
+
+    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
+    daily_love = get_daily_love()
+
+    for openId in openIds:
+        body = {
+            "touser": openId,  # 接收者 openId
+            "template_id": weather_template_id.strip(),  # 天气模板 ID
+            "url": "https://ww1xhqc-gemini-play-11.deno.dev/",  # 点击模板消息后跳转的链接
+            "data": {
+                "date": {
+                    "value": today_str  # 当前日期
+                },
+                "region": {
+                    "value": weather[0]  # 城市
+                },
+                "weather": {
+                    "value": weather[2]  # 天气类型
+                },
+                "temp": {
+                    "value": weather[1]  # 温度
+                },
+                "wind_dir": {
+                    "value": weather[3]  # 风向
+                },
+                "today_note": {
+                    "value": daily_love  # 每日情话
+                }
+            }
+        }
+        print(requests.post(url, json.dumps(body)).text)
+
+
+def weather_report(city):
+    # 1.获取access_token
+    access_token = get_access_token()
+    # 2. 获取天气
+    weather = get_weather(city)
+    print(f"天气信息： {weather}")
+    # 3. 发送消息
+    send_weather(access_token, weather)
+
+
+if __name__ == '__main__':
+    weather_report("驻马店")
+
